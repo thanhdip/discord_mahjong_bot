5dca72d650630668fb6816636e5600c231aef695
asdf <asdf@asdf.com>
2023-07-02 01:27:04 -0700

    add force to draw

--- cogs/mahjong.py
+++ cogs/mahjong.py
@@ -9,10 +9,12 @@ logger = getLogger(__name__)
 
 
 class MahjongDrawer:
-    WINDS = {"TON": "東", "NAN": "南", "SHAA": "西", "PEI": "北"}
-    TILE_BACK = "█ "
+    WINDS: dict[str, str] = {"TON": "東", "NAN": "南", "SHAA": "西", "PEI": "北"}
+    TILE_BACK: str = "█ "
+    DRAWN_USERS: list[str] = []
 
     def __init__(self) -> None:
+        self.DRAWN_USERS = []
         self.new_set()
 
     def reveal_tiles(self, num_reveal: Optional[int] = None) -> str:
@@ -48,7 +50,7 @@ class Mahjong(commands.Cog):
     async def winds(
         self,
         ctx: commands.Context,
-        members: commands.Greedy[discord.Member],
+        members: commands.Greedy[discord.Member] = None,
         *,
         force: str = None,
     ) -> None:
@@ -60,7 +62,7 @@ class Mahjong(commands.Cog):
             for member in members[:4]:
                 cur_tile = self.mahjong_drawer.reveal_next()
                 draw_msg += f"{member.name} drew: {cur_tile}\n"
-        draw_msg += "\n"         
+        draw_msg += "\n"
 
         drawn_tiles = self.mahjong_drawer.reveal_tiles()
         msg = f"""{draw_msg}Tiles:
@@ -71,14 +73,21 @@ class Mahjong(commands.Cog):
 
     @commands.command()
     async def draw(
-        self, ctx: commands.Context, members: commands.Greedy[discord.Member]
+        self,
+        ctx: commands.Context,
+        members: commands.Greedy[discord.Member] = None,
+        force: str = None,
     ) -> None:
         if not self.mahjong_drawer:
             await self.winds(ctx)
 
         user = ctx.author.display_name
+        if user in self.mahjong_drawer.DRAWN_USERS and force.lower() != "force":
+            await ctx.send("You've already drawn, {user}. Type it in again with force if you want to draw.")
+            return
+
         cur_tile = self.mahjong_drawer.reveal_next()
-        draw_msg = f"{user} drew: {cur_tile}"
+        draw_msg = f"{user} drew: {cur_tile}\n"
 
         if members:
             for member in members[:3]:
@@ -88,6 +97,7 @@ class Mahjong(commands.Cog):
         drawn_tiles = self.mahjong_drawer.reveal_tiles()
         draw_msg += f"""
         
+        Tiles:
         {drawn_tiles}
         """
 
